79b5f617355265222681185b5ac2278d5adfde99
Patrick Morrissey <holla@patrickmorrissey.com>
2020-07-16 09:50:01 -0400

    Set up RSS source; remove more unused

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Third Mind`,
+    description: `Randomized Prose`,
+    author: `@frightlab`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,13 +13,11 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `mythirdmind`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -27,6 +25,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-rss-feed`,
+      options: {
+        url: `http://patrickmorrissey.com/feed/`,
+        name: `PatrickMorrissey`,
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
