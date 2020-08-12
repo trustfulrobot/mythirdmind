f6e7d6213f81724879468decc85de6989e5fdab1
Patrick Morrissey <holla@patrickmorrissey.com>
2020-08-12 11:35:02 -0400

    Basic print titles to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,47 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
-const IndexPage = () => (
-  <>
-    blank start
-  </>
-)
+export const feedPatrickMorrissey = graphql`
+	{
+		allFeedPatrickMorrissey {
+	    edges {
+	      node {
+	        content {
+	          encoded
+	        }
+	        id
+	        link
+	        title
+	      }
+	    }
+	  }
+	}
+`
+
+function IndexPage({ data }) {
+
+	// const source_PatrickMorrissey = "test";
+	console.log(data.allFeedPatrickMorrissey.edges);
+
+	const source_PatrickMorrissey = data.allFeedPatrickMorrissey.edges.map(function(node, index){
+		return (
+			<li key={index}>
+				{node.node.title}
+			</li>
+		)
+	})
+
+
+	// const source_PatrickMorrissey = "test";
+
+	return (
+		<div id="MyThirdMind">
+			<h1>Here we go</h1>
+			<ul>
+				{source_PatrickMorrissey}
+			</ul>
+		</div>
+	)
+}
 
 export default IndexPage
