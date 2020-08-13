75e8f8c37e5ec18e39398ceb658da75610aa1002
Patrick Morrissey <holla@patrickmorrissey.com>
2020-08-13 10:19:53 -0400

    Function for stripping tags from strings

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,16 +14,55 @@ export const feedPatrickMorrissey = graphql`
 	        title
 	      }
 	    }
+	  },
+
+		allFeedKentuckyTheater {
+	    edges {
+	      node {
+	        content {
+	          encoded
+	        }
+	        id
+	        link
+	        title
+	      }
+	    }
 	  }
 	}
 `
 
+function stripTags(content) {
+	if ((content===null) || (content===''))
+    return false;
+  else
+	  content = content.toString();
+	const tempDOM = document.createElement("div");
+	tempDOM.innerHTML = content;
+	const strippedContent = tempDOM.textContent || tempDOM.innerText || "";
+  return strippedContent;
+}
+
 function IndexPage({ data }) {
 
 	const source_PatrickMorrissey = data.allFeedPatrickMorrissey.edges.map(function(post, index){
+		const sourceID = post.node.id;
+		const sourceTitle = stripTags(post.node.title);
+		const sourceLink = post.node.link;
+		const sourceContent = stripTags(post.node.content.encoded);
 		return (
-			<li key={index}>
-				{post.node.title}
+			<li key={sourceID}>
+				{sourceTitle}
+			</li>
+		)
+	})
+	const source_KentuckyTheater = data.allFeedKentuckyTheater.edges.map(function(post, index){
+		const sourceID = post.node.id;
+		const sourceTitle = stripTags(post.node.title);
+		const sourceLink = post.node.link;
+		const sourceContent = stripTags(post.node.content.encoded);
+		return (
+			<li key={sourceID}>
+				{sourceContent}
 			</li>
 		)
 	})
@@ -33,6 +72,9 @@ function IndexPage({ data }) {
 			<h1>Here we go</h1>
 			<ul>
 				{source_PatrickMorrissey}
+			</ul>
+			<ul>
+				{source_KentuckyTheater}
 			</ul>
 		</div>
 	)
