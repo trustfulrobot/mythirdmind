7c435473d03980b3d3a0ea244180f13ba7554711
Patrick Morrissey <holla@patrickmorrissey.com>
2020-08-14 11:04:59 -0400

    Remove your string functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,52 +57,28 @@ export const feedPatrickMorrissey = graphql`
 	}
 `
 
-function stripTags(content) {
-	if ((content===null) || (content===''))
-    return false;
-  else
-	  content = content.toString();
-	const tempDOM = document.createElement("div");
-	tempDOM.innerHTML = content;
-	const strippedContent = tempDOM.textContent || tempDOM.innerText || "";
-  return strippedContent;
-}
-
 function IndexPage({ data }) {
 
-	console.log(data.allFeedPatrickMorrissey.edges);
 
 	const source_PatrickMorrissey = data.allFeedPatrickMorrissey.edges.map(function(post, index){
 		const sourceID = post.node.id;
-		const sourceTitle = stripTags(post.node.title);
+		// const sourceTitle = stripTags(post.node.title);
+		const sourceTitle = post.node.title;
 		const sourceLink = post.node.link;
-		const sourceContent = stripTags(post.node.content.encoded);
+		// const sourceContent = stripTags(post.node.content.encoded);
+		const sourceContent = post.node.content.encoded;
 		return (
 			<li key={sourceID}>
 				{sourceTitle}
 			</li>
 		)
-	})
-	const source_KentuckyTheater = data.allFeedKentuckyTheater.edges.map(function(post, index){
-		const sourceID = post.node.id;
-		const sourceTitle = stripTags(post.node.title);
-		const sourceLink = post.node.link;
-		const sourceContent = stripTags(post.node.content.encoded);
-		return (
-			<li key={sourceID}>
-				{sourceContent}
-			</li>
-		)
-	})
+	});
 
 	return (
 		<div id="MyThirdMind">
 			<h1>Here we go</h1>
 			<ul>
 				{source_PatrickMorrissey}
-			</ul>
-			<ul>
-				{source_KentuckyTheater}
 			</ul>
 		</div>
 	)
